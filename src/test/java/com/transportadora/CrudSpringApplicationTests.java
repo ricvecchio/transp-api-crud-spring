49a34732b2d2a11c103cfdc064bbb19ab71a08f9
ricardo <ric.vecchio@gmail.com>
2025-01-04 11:54:39 -0300

    Rebuildando o projeto para corrigir erros de tests.

--- src/test/java/com/transportadora/CrudSpringApplicationTests.java
+++ src/test/java/com/transportadora/CrudSpringApplicationTests.java
@@ -3,7 +3,7 @@ package com.transportadora;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 class CrudSpringApplicationTests {
 
 	@Test
