be3b2ac286f8c0bd8e1867c5908f22ab460dbae0
ricardo <ric.vecchio@gmail.com>
2025-02-10 17:43:22 -0300

    Removendo @Autowired da classe EmailService.

--- src/main/java/com/transportadora/user/service/EmailService.java
+++ src/main/java/com/transportadora/user/service/EmailService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-    @Autowired
     private final JavaMailSender javaMailSender;
 
     @Value("${spring.mail.username}")
