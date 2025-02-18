369f4663d0c736dd0d9dc7a6ec82ae2977313a11
ricardo <ric.vecchio@gmail.com>
2025-02-18 18:30:10 -0300

    Liberando rota da hostinger.

--- src/main/java/com/transportadora/user/security/CorsConfig.java
+++ src/main/java/com/transportadora/user/security/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://saotomecatimesaotomecatime.com/")
+                .allowedOrigins("http://saotomecatimesaotomecatime.com")
                 .allowedMethods("GET", "POST", "DELETE", "PUT")
                 .allowedHeaders("*");
     }
