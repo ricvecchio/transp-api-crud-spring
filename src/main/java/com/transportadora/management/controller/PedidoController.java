2cb216dcbba211d1980434c0b456a2640b67950c
ricardo <ric.vecchio@gmail.com>
2025-03-06 00:13:52 -0300

    Incluindo as rotas "/api/clientes/**" e "/api/pedidos/**" para ("ADMIN", "USER").

--- src/main/java/com/transportadora/management/controller/PedidoController.java
+++ src/main/java/com/transportadora/management/controller/PedidoController.java
@@ -10,7 +10,6 @@ import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/transportadora/user/security/CorsConfig.java
+++ src/main/java/com/transportadora/user/security/CorsConfig.java
@@ -14,5 +14,4 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "DELETE", "PUT")
                 .allowedHeaders("*");
     }
-
 }
