87c16fd7ee294966e5717cb71fd98ba04a96eb36
ricardodelvecchio <ric.vecchio@gmail.com>
2025-04-07 16:32:20 -0300

    Adicionando tamanhos nas colunas da tabela Users.

--- src/main/java/com/transportadora/user/entities/User.java
+++ src/main/java/com/transportadora/user/entities/User.java
@@ -17,10 +17,19 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID idUser;
+
+    @Column(length = 100, nullable = false)
     private String name;
+
+    @Column(length = 100, nullable = false)
     private String email;
+
+    @Column(length = 50, nullable = false)
     private String username;
+
     private String password;
+
+    @Column(length = 20, nullable = false)
     private String permission;
 
 }
