3e488d25d8a65129c8439697f9792005d510e8f6
ricardodelvecchio <ric.vecchio@gmail.com>
2025-07-22 14:15:19 -0300

    Removendo a anotação @Validated da controller da DashboardController.

--- src/main/java/com/transportadora/management/controller/DashboardController.java
+++ src/main/java/com/transportadora/management/controller/DashboardController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@Validated
+//@Validated
 @RestController
 @CrossOrigin(origins = "https://saotomecatimesaotomecatime.com")
 @RequestMapping("/api/dashboard")
