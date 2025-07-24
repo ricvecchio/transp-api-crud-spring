78d211785094989376bfd07169cd0eef6ac02694
ricardodelvecchio <ric.vecchio@gmail.com>
2025-07-24 13:14:17 -0300

    testes

--- src/main/java/com/transportadora/management/controller/DashboardController.java
+++ src/main/java/com/transportadora/management/controller/DashboardController.java
@@ -20,7 +20,7 @@ public class DashboardController {
 
     private final DashboardService dashboardService;
 
-//    @GetMapping
+    //    @GetMapping
 //    public ResponseEntity<?> dashboard(
 //            @RequestParam(defaultValue = "0") @PositiveOrZero int page,
 //            @RequestParam(defaultValue = "10") @Positive @Max(100) int pageSize,
@@ -31,17 +31,12 @@ public class DashboardController {
 //    }
     @GetMapping
     public ResponseEntity<?> dashboard(
-            //@RequestParam(defaultValue = "0") @PositiveOrZero int page,
-            //@RequestParam(defaultValue = "10") @Positive @Max(100) int pageSize,
-            //@RequestParam(required = false) String filtro
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int pageSize,
             @RequestParam(defaultValue = "") String filter) {
-    ) {
         System.out.println("Entrou no método original");
         return dashboardService.dashboard(page, pageSize, filter);
     }
-
 
     // EXCLUIR TUDO PARA BAIXO
     private final UserService userService;
