779bb1ba4edd3768f6a68f49e141a863f7b4323a
ricardodelvecchio <ric.vecchio@gmail.com>
2025-07-25 15:41:03 -0300

    Ajustes na DashboardService.

--- src/main/java/com/transportadora/management/service/DashboardService.java
+++ src/main/java/com/transportadora/management/service/DashboardService.java
@@ -36,14 +36,15 @@ public class DashboardService {
     public DashboardDTO dashboard(int page, int pageSize, String filter) {
         System.out.println("[DashboardService] Entrou aqui /dashboard");
 
-        List<Object[]> topClientesResult = pedidoRepository.findTop5ClientesPorMesNative();
+        List<Object[]> topClientesResult = List.of(); //EXLUIR
+//        List<Object[]> topClientesResult = pedidoRepository.findTop5ClientesPorMesNative();
         System.out.println("Resultado topClientesResult = " + (topClientesResult == null ? "null" : topClientesResult.size()));
 
         List<Object[]> totaisPorMesResult = List.of(); //EXLUIR
 //        List<Object[]> totaisPorMesResult = pedidoRepository.findTotaisPorMes();
         System.out.println("Resultado totaisPorMesResult = " + (totaisPorMesResult == null ? "null" : totaisPorMesResult.size()));
 
-//        List<Object[]> topClientesResult = List.of(); //EXLUIR
+
 
 
         Map<String, List<ClienteGastoDTO>> clientesPorMes = new HashMap<>();
