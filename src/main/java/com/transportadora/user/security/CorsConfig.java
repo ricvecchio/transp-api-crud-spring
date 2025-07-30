b32fbad3564c40dbaba1300a06e22ec15273d33b
ricardodelvecchio <ric.vecchio@gmail.com>
2025-07-29 22:33:44 -0300

    Adicionando comentario para rodar Local.

--- src/main/java/com/transportadora/user/security/CorsConfig.java
+++ src/main/java/com/transportadora/user/security/CorsConfig.java
@@ -11,6 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://saotomecatimesaotomecatime.com")
+//                .allowedOrigins("*") // ==> APENAS PARA EXECUTAR LOCAL
                 .allowedMethods("GET", "POST", "DELETE", "PUT")
                 .allowedHeaders("*");
     }
